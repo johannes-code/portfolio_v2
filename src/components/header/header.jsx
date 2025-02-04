7d3ea385c2a58be1e2f690bc916b5e70d9fcf2b2
johannes-code <johannes24sfj@gmail.com>
2025-02-04 12:17:38 +0100

    made some styling changes, added logo right size

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,17 +2,22 @@ import data from "/src/locales/en.json";
 import styles from "./header.module.css";
 import { MediaHeader } from "./MediaHeader";
 
+
 export const Header = () => {
   console.log(data);
   return (
     <header className={styles.header}>
-      <ul className="flex gap-8 justify-end p-8">
+      <div className={styles.container}>
+        <div className={styles.innerHeader}></div>
+      <img className={styles.logoImage} src="/public/logo.svg" alt="logo" />
+      <ul className={styles.ul}>
         {data.header.map((item, index) => (
           <li key={item.name + index}>
             <a href={item.url}>{item.name}</a>
           </li>
         ))}
       </ul>
+      </div>
       {/* Create minor components if you want */}
       <MediaHeader />
     </header>
