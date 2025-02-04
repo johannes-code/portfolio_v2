e57eeca96638c8781cf6ae463944740a03f03a6f
johannes-code <johannes24sfj@gmail.com>
2025-02-04 09:29:50 +0100

    added OLD files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 
-import {t} from "./components/header/header.js"
+
 function App() {
   
   // console.log(header.home)
   return (
     <>
     <header>
-    {t}   
+        
     </header>
       
     </>
