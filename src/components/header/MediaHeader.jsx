4d049f165f62c9f143fdefd11454b48de929aa7a
johannes-code <johannes24sfj@gmail.com>
2025-02-05 10:03:59 +0100

    media header is placed, headerlinks are stubborn(not placed correctly yet)

--- src/components/header/MediaHeader.jsx
+++ src/components/header/MediaHeader.jsx
@@ -1,28 +1,29 @@
-import styles from "./header.module.css";
 
 export const MediaHeader = () => {
 
   return (
     <>
-      <div className="media_header absolute left-8 top-0">
-        <div className="media_line h-50 w-[1px] bg-white" />
-        
-        <img
-          className="media_icon absolute left-8 h-50 w-50 top-34"
-          src="/src/images/icons/discord.svg"
-          alt="discordicon"
-        />
-        <img
-          className="media_icon absolute left-8 h-50 w-50 top-46"
-          src="/src/images/icons/github.svg"
-          alt="githubicon"
-        />
-        <img 
-        className="media_icon absolute left-8 h-50 w-50 top-54" 
-        src="/src/images/icons/email.svg" 
-        alt="emailicon" 
-        />
-        
+      <div className="media_header fixed left-8 top-0 flex flex-col justify-center items-center">
+        <div className="media_line h-50 w-[1px] bg-gray-500 mb-4" />
+          
+          <div className ="flex flex-col space-y-4">
+            <img
+              className="media_icon .h-3rem .w-3rem"
+              src="/src/images/icons/discord.svg"
+              alt="discordicon"
+            />
+            <img
+              className="media_icon .h-3rem .w-3rem"
+              src="/src/images/icons/github.svg"
+              alt="githubicon"
+            />
+            <img 
+            className="media_icon .h-3rem .w-3rem" 
+            src="/src/images/icons/email.svg" 
+            alt="emailicon" 
+            />
+          </div>
+      
       </div>
     </>
   );
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,22 +2,22 @@ import data from "/src/locales/en.json";
 import styles from "./header.module.css";
 import { MediaHeader } from "./MediaHeader";
 
-
 export const Header = () => {
   console.log(data);
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-          <div className={styles.innerHeader}></div>
-            <img className={styles.logoImage} src="/logo.svg" alt="logo" />
-              <ul className={styles.ul}>
-                {data.header.map((item, index) => (
-                  <li key={item.name + index}>
-                    <a href={item.url}>{item.name}</a>
-                  </li>
-                ))}
-              </ul>
+        <div className={styles.innerHeader}>
+          <img className={styles.logoImage} src="/logo.svg" alt="logo" />
+          <ul className={styles.navItems}>
+            {data.header.map((item, index) => (
+              <li key={item.name + index}>
+                <a href={item.url}>{item.name}</a>
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
       {/* Create minor components if you want */}
       <MediaHeader />
     </header>
