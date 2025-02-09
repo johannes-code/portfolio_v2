652fc3967e2c876a8374baf72fffb07c42458205
johannes-code <johannes24sfj@gmail.com>
2025-02-09 22:09:47 +0100

    changed how quotes renders, quotes section is good

--- src/components/quote/Quote.jsx
+++ src/components/quote/Quote.jsx
@@ -1,7 +1,20 @@
 import styles from "./quote.module.css";
 import data from "/src/locales/en.json";
+import { useState, useEffect } from "react";
+
 export const Quote = () => {
-  const quoteData = data.pages.home.quote;
+  const [quoteData, setQuote] = useState({ text: "", author: "" });
+
+  useEffect(() => {
+    getRandomQuote();
+  }, []);
+
+  const getRandomQuote = () => {
+    // Extract the quotes object from the JSON file
+    const quotes = Object.values(data.quotes);
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setQuote(quotes[randomIndex]);
+  };
 
   return (
     <figure className={styles.quote}>
