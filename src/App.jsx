04fd4a7268684599e69e7bf979c053cdd1e31a1e
scrapjo <darklord85@gmail.com>
2025-02-10 11:33:43 +0100

    Startted implementing projects sectrion. h2 renndering , but needs style change project boxes needs to render

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Header } from "./components/header/header";
 import { Hero } from "./components/hero/hero";
+import { Projects } from "./components/projects/Projects";
 import { Quote } from "./components/quote/Quote";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className="container_content">
         <Hero />
         <Quote />
+        <Projects />
       </div>
     </>
   );
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,28 @@
-
+import data from "/src/locales/en.json";
+import styles from "./projects.module.css";
 
 export const Projects = () => {
-
+    const projectPageData = data.pages.home.projects;
+    const projectData = data.projectsExpanded;
     return(
-        
+        <section className={styles.projects}>
+            <div className={styles.projects_header}>
+                <h2 className={styles.h2}> {projectPageData.title} </h2>
+                <a className={styles.projects_link} href="/projects">{projectPageData.button} </a>
+                </div>
+            <div className={styles.project_list}>
+                <div className={styles.project}>{projectData.name}</div>
+                    <img src="{projectData.screenshot}" alt="" className={styles.project_image}/>
+                    <ul className={styles.project_techs}>
+                        <li className={styles.project_tech}>{projectData.tech}</li>
+                    </ul>
+                <div className={styles.project_content}>
+                    <div className={styles.project_name}></div>
+                    <div className={styles.project_description}></div>
+                    <div className={styles.project_links}></div>
+                </div>
+            </div>            
+
+        </section>
     )
 }
