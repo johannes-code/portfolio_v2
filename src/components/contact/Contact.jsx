40de0c745e182e4ed3680b9bf1d55b125da6d713
scrapjo <darklord85@gmail.com>
2025-02-11 21:38:00 +0100

    tweaking the contact section, still needs more work to render correctly

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,15 +10,16 @@ export const Contact = () => {
       <div className={styles.contacts_content}>
         <p className={styles.contacts_description}>{contacts.text}</p>
         <h3 className={styles.contacts_title}>{contacts.media}</h3>
+        
         <div className={styles.contacts_list}>
-          <a
-            className={styles.contact}
-            href=""
-            src="../images/icons/discord.svg"
-          >
-            <div className={styles.contact_name}></div>
-          </a>
-          <a href="" className="contact"></a>
+          <a className={styles.contact} href="https://discord.com/users/scrapjo"></a>
+            <img src={data.mediaIcon.discord} alt="" />
+            <div className={styles.contact_name}>{data.contactinfo.discord}</div>
+          
+          <a className={styles.contact} href="mailto:johannes@gjeset.no"></a>
+            <img src={data.mediaIcon.email} alt="" />
+            <div className={styles.contact_name}>{data.contactinfo.mail}</div>
+        
         </div>
       </div>
     </section>
