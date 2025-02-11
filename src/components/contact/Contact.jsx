bb45a2982a65681bdec497f26dd318be81cabc4c
johannes-code <johannes24sfj@gmail.com>
2025-02-11 13:19:18 +0100

    started setting up the contact section

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,22 @@
+export const Contact = () => {
+  return (
+    <section className={styles.contacts}>
+      <h2 className={styles.h2}></h2>
+      <p className={styles.contacts_description}></p>
+      <div className={styles.contacts_content}>
+        <h3 className={styles.contacts_title}></h3>
+        <div className={styles.contacts_list}>
+          <a
+            className={styles.contact}
+            href=""
+            img
+            src="../images/icons/discord.svg"
+          >
+            <div className={styles.contact_name}></div>
+          </a>
+          <a href="mailto:johannes@gjeset.no" className="contact"></a>
+        </div>
+      </div>
+    </section>
+  );
+};
