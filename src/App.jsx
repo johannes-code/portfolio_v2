94013d85535fd14eb445bc39708a5d568cf098bc
johannes-code <johannes24sfj@gmail.com>
2025-02-11 14:51:03 +0100

    corrected font, set up contacts section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import { Header } from "./components/header/header";
-// import { Hero } from "./components/hero/hero";
-// import { Projects } from "./components/projects/Projects";
-// import { Quote } from "./components/quote/Quote";
-// import { Skills } from "./components/skills/Skills";
+import { Hero } from "./components/hero/hero";
+import { Projects } from "./components/projects/Projects";
+import { Quote } from "./components/quote/Quote";
+import { Skills } from "./components/skills/Skills";
 import { About } from "./components/about/About";
+import { Contact } from "./components/contact/Contact";
 
 function App() {
   // console.log(header.home)
@@ -12,11 +13,12 @@ function App() {
     <>
       <Header />
       <div className="container_content">
-        {/* <Hero /> */}
-        {/* <Quote /> */}
-        {/* <Projects /> */}
-        {/* <Skills /> */}
+        <Hero />
+        <Quote />
+        <Projects />
+        <Skills />
         <About />
+        <Contact />
       </div>
     </>
   );
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,15 +1,19 @@
+import styles from "./contact.module.css";
+import data from "../../locales/en.json";
+
 export const Contact = () => {
+  const contacts = data.pages.home.contacts;
+
   return (
     <section className={styles.contacts}>
-      <h2 className={styles.h2}></h2>
-      <p className={styles.contacts_description}></p>
+      <h2 className={styles.h2}>{contacts.title}</h2>
       <div className={styles.contacts_content}>
-        <h3 className={styles.contacts_title}></h3>
+        <p className={styles.contacts_description}>{contacts.text}</p>
+        <h3 className={styles.contacts_title}>{contacts.media}</h3>
         <div className={styles.contacts_list}>
           <a
             className={styles.contact}
             href=""
-            img
             src="../images/icons/discord.svg"
           >
             <div className={styles.contact_name}></div>
