672188fd65c47c618e66ebf14366bf0c603c8de3
johannes-code <johannes24sfj@gmail.com>
2025-02-25 14:15:15 +0100

    porjectExpanded is now rendering all data. -needs styling

--- src/landing/projects/Projects.jsx
+++ src/landing/projects/Projects.jsx
@@ -4,83 +4,87 @@ import button from "../../components/button/button.module.css";
 import { Link } from "react-router-dom";
 
 export const Projects = () => {
-  <projectsExpanded data={data} />;
   const projectPageData = data.pages.home.projects;
   const projectData = data.projectsExpanded;
   const skillData = data.skill;
 
   return (
     <section className={styles.projects} id="projects">
-      <div className={styles.projects_header}>
-        <h2 className={styles.h2}>{projectPageData.title}</h2>
-        <Link className={styles.projects_link} to="/project/">
-          {projectPageData.button}
-        </Link>
-      </div>
-      <div className={styles.project_list}>
-        {projectData.map((project, projectIndex) => (
-          <div key={project.name + projectIndex} className={styles.project}>
-            {/* 1. Picture */}
-            <img
-              src={project.screenshot}
-              alt={project.name}
-              className={styles.project_image}
-            />
+      <div>
+        <div className={styles.projects_header}>
+          <h2 className={styles.h2}>{projectPageData.title}</h2>
+          <Link className={styles.projects_link} to="/project/">
+            {projectPageData.button}
+          </Link>
+        </div>
+        <div className={styles.project_list}>
+          {projectData.map((project, projectIndex) => (
+            <div key={project.name + projectIndex} className={styles.project}>
+              {/* 1. Picture */}
+              <img
+                src={project.screenshot}
+                alt={project.name}
+                className={styles.project_image}
+              />
 
-            {/* 2. Project Name */}
-            <h3>{project.name}</h3>
+              {/* 2. Project Name */}
+              <h3>{project.name}</h3>
 
-            {/* 3. Tech Used (Skills) */}
-            <ul className={styles.project_techs}>
-              {project.tech && project.tech.length > 0 ? (
-                project.tech.map((techItem, techIndex) => {
-                  if (typeof techItem !== "string" || !techItem.includes(".")) {
-                    console.error("Invalid tech item:", techItem);
-                    return null;
-                  }
-
-                  const [category, indices] = techItem.split(".");
-                  if (!skillData[category]) {
-                    console.error("Invalid category:", category);
-                    return null;
-                  }
-
-                  const indexArray = indices.split(",");
-
-                  return indexArray.map((index, skillIndex) => {
-                    const skill = skillData[category][index];
-                    if (!skill) {
-                      console.error(`Invalid skill at ${category}[${index}]`);
+              {/* 3. Tech Used (Skills) */}
+              <ul className={styles.project_techs}>
+                {project.tech && project.tech.length > 0 ? (
+                  project.tech.map((techItem, techIndex) => {
+                    if (
+                      typeof techItem !== "string" ||
+                      !techItem.includes(".")
+                    ) {
+                      console.error("Invalid tech item:", techItem);
                       return null;
                     }
 
-                    return (
-                      <li
-                        key={`${skill}-${techIndex}-${skillIndex}`}
-                        className={styles.project_tech}
-                      >
-                        {skill}
-                      </li>
-                    );
-                  });
-                })
-              ) : (
-                <li>No skills listed</li>
-              )}
-            </ul>
+                    const [category, indices] = techItem.split(".");
+                    if (!skillData[category]) {
+                      console.error("Invalid category:", category);
+                      return null;
+                    }
 
-            {/* 4. Description */}
-            <p>{project.description}</p>
+                    const indexArray = indices.split(",");
 
-            {/* 5. Button/Link */}
-            <Link
-              to={`/project/${projectIndex}`}
-              className={button._button_button_primary}
-            >
-              View Project
-            </Link>
-          </div>
-        ))}
+                    return indexArray.map((index, skillIndex) => {
+                      const skill = skillData[category][index];
+                      if (!skill) {
+                        console.error(`Invalid skill at ${category}[${index}]`);
+                        return null;
+                      }
+
+                      return (
+                        <li
+                          key={`${skill}-${techIndex}-${skillIndex}`}
+                          className={styles.project_tech}
+                        >
+                          {skill}
+                        </li>
+                      );
+                    });
+                  })
+                ) : (
+                  <li>No skills listed</li>
+                )}
+              </ul>
+
+              {/* 4. Description */}
+              <p>{project.description}</p>
+
+              {/* 5. Button/Link */}
+              <Link
+                to={`/project/${projectIndex}`}
+                className={button._button_button_primary}
+              >
+                View Project
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/landing/projects/projectsExpanded/ProjectsExpanded.jsx
+++ src/landing/projects/projectsExpanded/ProjectsExpanded.jsx
@@ -2,35 +2,56 @@ import { useState } from "react";
 import styles from "./projectsExpanded.module.css";
 import PropTypes from "prop-types";
 import { RenderTechSkills } from "../../../components/functions/RenderTechSkills";
-import { RenderArrayContent } from "../../../components/functions/RenderArrayContent";
+import data from "../../../locales/en.json";
 
-<RenderArrayContent />;
-
-export const ProjectsExpanded = (data, techArray) => {
+export const ProjectsExpanded = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const currentProject = data.projectsExpanded[currentIndex];
+  const currentProject = data?.projectsExpanded[currentIndex];
+
+  if (!currentProject) {
+    return <div>No project data available.</div>;
+  }
 
   return (
     <div className={styles.projectsExpanded}>
       <div>
         <h2>{currentProject.name}</h2>
         <p>{currentProject.description}</p>
-
         <RenderTechSkills
-          techArray={techArray}
+          techArray={currentProject.tech}
           ulClassName={styles.project_techs}
           liClassName={styles.project_tech}
         />
-
-        <p>{currentProject.tech}</p>
         <p>{currentProject.link}</p>
         <img src={currentProject.screenshot} alt={currentProject.name} />
-
-        {RenderArrayContent(currentProject.long_desc)}
-        {RenderArrayContent(currentProject.learned)}
-        {RenderArrayContent(currentProject.missed)}
-        {RenderArrayContent(currentProject.why)}
-
+        {currentProject.long_desc &&
+          currentProject.long_desc.map((item, index) => (
+            <div key={index}>
+              <h3>{item.heading}</h3>
+              <p>{item.content}</p>
+            </div>
+          ))}
+        {currentProject.learned &&
+          currentProject.learned.map((item, index) => (
+            <div key={index}>
+              <h3>{item.heading}</h3>
+              <p>{item.content}</p>
+            </div>
+          ))}
+        {currentProject.missed &&
+          currentProject.missed.map((item, index) => (
+            <div key={index}>
+              <h3>{item.heading}</h3>
+              <p>{item.content}</p>
+            </div>
+          ))}
+        {currentProject.why &&
+          currentProject.why.map((item, index) => (
+            <div key={index}>
+              <h3>{item.heading}</h3>
+              <p>{item.content}</p>
+            </div>
+          ))}
         <button
           onClick={() =>
             setCurrentIndex(
@@ -50,9 +71,34 @@ ProjectsExpanded.propTypes = {
     projectsExpanded: PropTypes.arrayOf(
       PropTypes.shape({
         name: PropTypes.string.isRequired,
+        tech: PropTypes.arrayOf(PropTypes.string),
         description: PropTypes.string.isRequired,
-
-        // Add other properties as needed
+        link: PropTypes.string,
+        screenshot: PropTypes.string,
+        long_desc: PropTypes.arrayOf(
+          PropTypes.shape({
+            heading: PropTypes.string.isRequired,
+            content: PropTypes.string.isRequired,
+          })
+        ),
+        learned: PropTypes.arrayOf(
+          PropTypes.shape({
+            heading: PropTypes.string.isRequired,
+            content: PropTypes.string.isRequired,
+          })
+        ),
+        missed: PropTypes.arrayOf(
+          PropTypes.shape({
+            heading: PropTypes.string.isRequired,
+            content: PropTypes.string.isRequired,
+          })
+        ),
+        why: PropTypes.arrayOf(
+          PropTypes.shape({
+            heading: PropTypes.string.isRequired,
+            content: PropTypes.string.isRequired,
+          })
+        ),
       })
     ).isRequired,
   }).isRequired,
