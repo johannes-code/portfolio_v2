76c79c468a6686746a9d466023dce4d329a22dc4
johannes-code <johannes24sfj@gmail.com>
2025-03-03 10:38:49 +0100

    nav links works when coming from a sub page

--- src/landing/LandingPage.jsx
+++ src/landing/LandingPage.jsx
@@ -4,8 +4,21 @@ import { Projects } from "./projects/Projects";
 import { Skills } from "./skills/Skills";
 import { About } from "./about/About";
 import { Contact } from "./contact/Contact";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 export function LandingPage() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.slice(1));
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
+
   return (
     <div className="container_content">
       <Hero />
